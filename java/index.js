7e9203cb533804efc7354e8a694df00752428e4e
thomaaaaaaa <152179717+thomaaaaaaa@users.noreply.github.com>
2024-03-13 13:02:42 +0100

    Add files via upload
    1e versie!

---
+++ java/index.js
@@ -0,0 +1,40 @@
+// function showHint(){
+//     hint1.showContent = "wil je mijn koffiepasje lenen?"
+//     setTimeout(hideHint, 4000)
+// }
+
+// function hideHint(){
+//     hint1.textContent = " "
+//     setTimeout(hideHint, 4000)
+// }
+
+// showHint()
+// let timer = setInterval(countSeconds, 1000)
+// function countSeconds(){
+// }
+
+
+// TESTEN
+
+// const vol = 7
+// let kaasGegeten = 0
+
+// while(kaasGegeten < vol){
+//     //blijf eten
+//     kaasGegeten = kaasGegeten + Math.random()
+//     console.log(kaasGegeten)
+// }
+
+// for (let honger = 0; honger <= 5; honger++) {
+//     console.log(honger)
+// }
+
+const eten = ["Boter", "Kaas", "Eieren", "Meel", "Suiker"]
+
+const eersteEten = eten[0]
+const middelsteEten = eten[3]
+const laatsteEten = eten[5]
+
+console.log(eersteEten)
+console.log(middelsteEten)
+console.log(laatsteEten)
